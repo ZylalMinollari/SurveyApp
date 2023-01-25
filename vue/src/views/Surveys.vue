36e91f7c6be84e078756edd5a9705b996bebbddb
ZylalMinollari <zminollari@gmail.com>
2023-01-25 10:27:37 +0100

    first commit with implementation of register and login

---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,32 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <PageComponent title="Surveys">
+    <router-link
+      v-for="survey in surveys"
+      :key="survey.id"
+      :to="{ name: 'SurveyView', params: { id: survey.id } }"
+      class="
+        block
+        py-4
+        px-6
+        shadow-md
+        bg-white
+        mb-5
+        rounded-md
+        hover:bg-blue-50
+        cursor-pointer
+      "
+    >
+      <h4 class="text-lg font-bold">{{ survey.title }}</h4>
+      <div v-html="survey.description"></div>
+    </router-link>
+  </PageComponent>
+</template>
+
+<script setup>
+import store from "../store";
+import { computed } from "vue";
+import PageComponent from "../components/PageComponent.vue";
+
+const surveys = computed(() => store.state.surveys);
+</script>
