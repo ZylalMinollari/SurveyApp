fa7762c52ed48c8ab85cae739b6b1edd3d2fe18a
ZylalMinollari <zminollari@gmail.com>
2023-01-30 11:32:43 +0100

    Get surveys from back

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,188 +1,6 @@
 import {createStore} from "vuex";
 import axiosClient from "../axios";
 
-const tmpSurveys = [
-  {
-    id: 100,
-    title: "Laravel 8",
-    slug: "laravel-8",
-    status: "draft",
-    image: "https://www.giantbomb.com/a/uploads/square_medium/1/19745/889857-the_last_doujutsu___itachi_by_kuja_trance.png",
-    description: "Laravel is a web application framework with expressive",
-    created_at: "2021-12-20 18:00:00",
-    updated_at: "2021-12-20 18:00:00",
-    expire_date: "2021-12-31 18:00:00",
-    questions: [
-      {
-        id: 101,
-        type: "select",
-        question: "Favourite framework ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Laravel"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      },
-      {
-        id: 102,
-        type: "select",
-        question: "Favourite Language ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Laravel"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      },
-    ]
-  },
-  {
-    id: 103,
-    title: "Vue",
-    slug: "vue",
-    status: "draft",
-    image: "https://www.giantbomb.com/a/uploads/square_medium/1/19558/896503-7261_itachi004_super.jpg",
-    description: "Laravel is a web application framework with expressive,",
-    created_at: "2021-12-20 18:00:00",
-    updated_at: "2021-12-20 18:00:00",
-    expire_date: "2021-12-31 18:00:00",
-    questions: [
-      {
-        id: 104,
-        type: "checkbox",
-        question: "Favourite framework ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Javascript"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      }
-    ]
-  },
-  {
-    id: 105,
-    title: "Vue",
-    slug: "vue",
-    status: "draft",
-    image: "https://www.giantbomb.com/a/uploads/square_medium/1/19745/889825-itachi___represent_by_h0shii.jpg",
-    description: "Laravel is a web application framework with expressive,",
-    created_at: "2021-12-20 18:00:00",
-    updated_at: "2021-12-20 18:00:00",
-    expire_date: "2021-12-31 18:00:00",
-    questions: [
-      {
-        id: 106,
-        type: "radio",
-        question: "Favourite framework ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Javascript"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      }
-    ]
-  },
-  {
-    id: 107,
-    title: "Vue",
-    slug: "vue",
-    status: "draft",
-    image: "",
-    description: "Laravel is a web application framework with expressive,",
-    created_at: "2021-12-20 18:00:00",
-    updated_at: "2021-12-20 18:00:00",
-    expire_date: "2021-12-31 18:00:00",
-    questions: [
-      {
-        id: 108,
-        type: "radio",
-        question: "Favourite framework ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Javascript"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      }
-    ]
-  },
-  {
-    id: 109,
-    title: "Vue",
-    slug: "vue",
-    status: "draft",
-    image: "",
-    description: "Laravel is a web application framework with expressive,",
-    created_at: "2021-12-20 18:00:00",
-    updated_at: "2021-12-20 18:00:00",
-    expire_date: "2021-12-31 18:00:00",
-    questions: [
-      {
-        id: 110,
-        type: "radio",
-        question: "Favourite framework ?",
-        description : null,
-        data: {
-          options: [
-            {
-              uuid: "ecc34c24-0eaf-4628-9146-c057c1a3ddfb", text: "Javascript"
-            },
-            {
-              uuid: "82a41572-4525-4d6d-8729-62121df69da6", text: "Node"
-            },
-            {
-              uuid: "b604bdc7-cbf6-4f23-8041-6666f323e881", text: "django"
-            }
-          ]
-        }
-      }
-    ]
-  }
-
-]
 const store = createStore({
   state: {
     user: {
@@ -193,7 +11,11 @@ const store = createStore({
       data: {},
       loading: false,
     },
-    surveys: [...tmpSurveys],
+    surveys: {
+      loading: false,
+      links: [],
+      data: [],
+    },
     questionTypes: ["text","select","radio","checkbox","textarea"],
   },
   getters: {},
@@ -237,7 +59,14 @@ const store = createStore({
       }
       return response;
     },
-
+    getSurveys({ commit }) {
+      commit('setSurveysLoading', true)
+      return axiosClient.get("/survey").then((res) => {
+        commit('setSurveysLoading', false)
+        commit("setSurveys", res.data);
+        return res;
+      });
+    },
     deleteSurvey({ }, id) {
       return axiosClient.delete(`/survey/${id}`);
     },
@@ -277,13 +106,19 @@ const store = createStore({
     //     }
     //     return s;
     //   });
-    // },
+    // }, //setSurveysLoading
 
     setCurrentSurveyLoading: (state, loading) => {
       state.currentSurvey.loading = loading;
     },
+    setSurveysLoading: (state, loading) => {
+      state.surveys.loading = loading;
+    },
     setCurrentSurvey: (state, survey) => {
       state.currentSurvey.data = survey.data;
+    },
+    seSurveys: (state, surveys) => {
+      state.surveys.data = surveys.data;
     },
   },
   modules: {},
