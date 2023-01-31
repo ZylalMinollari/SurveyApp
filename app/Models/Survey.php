bb60645b46e2f657a27a44285bcf77c31dc40736
ZylalMinollari <zminollari@gmail.com>
2023-01-31 18:00:44 +0100

    Backend implementation for survey like store question,get surveys store answers

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use App\Models\SurveyQuestion;
 use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Spatie\Sluggable\HasSlug;
 
 class Survey extends Model
 {
     use HasFactory,HasSlug;
+    const TYPE_TEXT = 'text';
+    const TYPE_TEXTAREA = 'textarea';
+    const TYPE_SELECT = 'select';
+    const TYPE_RADIO = 'radio';
+    const TYPE_CHECKBOX = 'checkbox';
 
     protected $fillable = ['user_id','title','slug','status','image','description','expire_date'];
 
@@ -21,4 +27,7 @@ class Survey extends Model
             ->saveSlugsTo('slug');
     }
 
+    public function questions() {
+        return $this->hasMany(SurveyQuestion::class);
+    }
 }
