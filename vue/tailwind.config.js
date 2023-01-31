63a65aea0991f450af820bc894fa8d19cc73c91f
ZylalMinollari <zminollari@gmail.com>
2023-01-31 09:54:20 +0100

    Working in notification and pagination

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        from: {
+          transform: "translateY(-0.75rem)",
+          opacity: "0",
+        },
+
+        to: {
+          transform: "translateY(0rem)",
+          opacity: "1",
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.2s ease-in-out both",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
