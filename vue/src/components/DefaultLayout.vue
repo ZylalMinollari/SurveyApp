38f0793090c13a8bee666e9d548d4c9fa79d5896
ZylalMinollari <84593674+ZylalMinollari@users.noreply.github.com>
2023-01-31 17:51:31 +0100

    Revert "Working in notification and pagination"

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -151,7 +151,6 @@
     </Disclosure>
 
     <router-view></router-view>
-    <!-- <Notification></Notification> -->
   </div>
 </template>
 
@@ -169,7 +168,6 @@ import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
 import { useStore } from "vuex";
 import { computed } from "vue";
 import { useRouter } from "vue-router";
-import Notification from "./Notification.vue";
 
 const navigation = [
   { name: "Dashboard", to: { name: "Dashboard" } },
@@ -188,8 +186,7 @@ export default {
     BellIcon,
     MenuIcon,
     XIcon,
-    Notification
-},
+  },
   setup() {
     const store = useStore();
     const router = useRouter();
